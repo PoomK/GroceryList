977cf06d6bfb6748a452ea7c24b8c7e09c8d327b
PoomK <pittawas.kijoran@gmail.com>
2023-02-25 14:47:44 +0000

    WebScraping for waitrose

---
+++ WebScraping.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+groceryItem = input("Enter grocery product: ")
+
+def scrapeWaitrose(item):
+    waitroseWebsite = f"https://www.waitrose.com/ecom/shop/search?&searchTerm={item}"
+
+    page = requests.get(waitroseWebsite)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    price_element = soup.find('span', {'class': 'itemPrice___ieIBH'})
+    price = price_element.find('span').find('span').text
+
+    product_name_element = soup.find('span', {'class': 'name___h83Rn ellipses___15kID'})
+    product_name = product_name_element.text
+
+    print(product_name)
+    print(str(price))
+
+scrapeWaitrose(groceryItem)
